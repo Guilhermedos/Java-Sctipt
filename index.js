d1953acb3d4032c393857dcd9ac82a828cc29b26
Random <159532360+Guilhermedos@users.noreply.github.com>
2024-04-05 08:38:55 -0300

    iniciandojava-script
    bora

---
+++ index.js
@@ -0,0 +1,12 @@
+const nome = "juliana"; // String
+
+console.log(nome)
+const nome2 = "nohellton da baixinha";
+console.log(nome,nome2)
+const nome3 = "Guilherme";
+console.log(nome,nome2,nome3)
+const nome4 = "maycon";
+console.log(nome,nome2,nome3,nome4)
+const nome5 = "kevin";
+console.log(nome,nome2,nome3,nome4,nome5)
+
