385f5d3e84949782a369ad43039a0e366fd6b06a
sandrorcmm <159532360+Guilhermedos@users.noreply.github.com>
2024-04-22 05:08:00 -0300

    progredind javasscripts22-04
    chama chama

---
+++ for.js
@@ -0,0 +1,16 @@
+const = escola [
+    {
+        nome: "yamal",
+        idade: 15,
+        sala: 1},
+    {
+        nome: "vinijr",
+        idade: 16,
+        sala:2},
+    {
+        nome: Guilherme
+        idade: 17
+        sala:3,}
+for(let i = 0; i < escola.lenght; i++){
+    console.log(escola[i].nome)
+}
